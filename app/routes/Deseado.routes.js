2d93eca1850f254cb04d018dfd23dcfc3bd522a3
palaciosfernan <134438027+palaciosfernan@users.noreply.github.com>
2024-03-14 09:04:48 -0600

    Update Deseado.routes.js

--- app/routes/Deseado.routes.js
+++ app/routes/Deseado.routes.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const deseosController = require("../controllers/Deseados.controller");
+const rateLimit = require("express-rate-limit");
 
-router.get("/deseos", deseosController.obtenerElementosDeseados);
+const deseosLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hora
+    max: 5, // límite de 5 peticiones por IP por hora
+    message: "Demasiadas peticiones para esta acción, intenta nuevamente más tarde"
+});
 
-router.get("/deseos/:id", deseosController.obtenerElementoDeseadoPorId);
+router.get("/deseos", deseosLimiter, deseosController.obtenerElementosDeseados);
 
-router.post("/deseos", deseosController.agregarElementoDeseado);
+router.get("/deseos/:id", deseosLimiter, deseosController.obtenerElementoDeseadoPorId);
 
-router.put("/deseos/:id", deseosController.actualizarElementoDeseado);
+router.post("/deseos", deseosLimiter, deseosController.agregarElementoDeseado);
 
-router.delete("/deseos/:id", deseosController.eliminarElementoDeseado);
+router.put("/deseos/:id", deseosLimiter, deseosController.actualizarElementoDeseado);
+
+router.delete("/deseos/:id", deseosLimiter, deseosController.eliminarElementoDeseado);
 
 module.exports = router;
